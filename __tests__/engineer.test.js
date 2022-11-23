1367d0db5e822cfc32ff5051cb941383611ad579
Sean Collins <iseanc@hotmail.com>
2022-11-22 17:24:00 -0700

    completed prompts and inputted data is written to a teams array

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -4,7 +4,7 @@ const Engineer = require('../lib/engineer');
 
 describe('Engineer', () => {
   describe("Initialization", () => {
-    it("should return an object containing undefined 'name', 'id', and 'email' properties when called with the 'new' keyword with no arguments", () => {
+    it("should return an object containing undefined 'name', 'id', 'email' and 'position' properties when called with the 'new' keyword with no arguments", () => {
       const e = new Engineer();
 
       expect(e).toEqual(
@@ -12,15 +12,14 @@ describe('Engineer', () => {
           name: undefined,
           id: undefined, 
           email: undefined, 
-          githubId: undefined 
-        })
-      );
+          position: "Engineer",
+          githubId: undefined}));
     });
 
     it("should return an object with 'name', 'id', 'email', and 'githubId' properties populated with values when called with the 'new' keyword with values supplied to arguments", () => {
       const e = new Engineer("Ed Engineer", "234", "someengineer@dcompany.com", "appmaker");
 
-      expect(e).toEqual(expect.objectContaining({ name: "Ed Engineer", id: "234", email: "someengineer@dcompany.com", githubId: "appmaker" }));
+      expect(e).toEqual(expect.objectContaining({ name: "Ed Engineer", id: "234", email: "someengineer@dcompany.com", position: "Engineer", githubId: "appmaker" }));
     });
   });
 });
--- index.js
+++ index.js
@@ -1,77 +1,128 @@
+// required Node.JS modules
 const fs = require('fs');
 const inquirer = require('inquirer');
 var http = require('http');
+
+// required user-defined modules
 const {questionsEmployee, 
   questionsManager, 
   questionsEngineer, 
   questionsIntern, questionAddTeamMember} = require('./lib/prompts');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       message: 'Enter manager\'s name: ',
-//       name: 'managerName',
-//     },
-//     {
-//       type: 'input',
-//       message: 'Enter manager\'s Employee ID: ',
-//       name: 'managerEmployeeId',
-//     },
-//     {
-//       type: 'input',
-//       message: 'Enter manager\'s Email Address: ',
-//       name: 'managerEmail',
-//     },
-//     {
-//       type: 'input',
-//       message: 'Enter manager \'s Office Number: ',
-//       name: 'managerOfficeNumber',
-//     },
-//     {
-//       type: 'list',
-//       message: 'What is your preferred method of communication?',
-//       name: 'contact',
-//       choices: ['email', 'phone', 'telekinesis'],
-//     },
-//   ])
-//   .then((response) =>
-//     response.confirm === response.password
-//       ? console.log('Success!')
-//       : console.log('You forgot your password already?!')
-//   );
+// Constants for unchanging data items
+const githubUrl = 'https://github.com/';
 
-  // VARIABLES/CONSTANTS WISHLIST
-  //------------------------------- 
-
-  employees = [];
+// Variables for changing data items
+let team = [];
 
 
-  //FUNCTION WISHLIST
-  //------------------------------- 
 
-  // function main() {}
-  function main() {
-    console.log('Creating a new team');
-    addManager();
-  }
 
-  // function addEmployee() {}
+// Initiate application functionality
+function main() {
+  // Welcome message
+  console.log('Creating a new team');
 
-  function addManager() {
-    console.log('Enter team manager information...');
+  // First "team member" is a Manager
+  addManager();
+}
 
-    inquirer.prompt(questionsManager).
-    then((answers) => {
-      console.log(answers);
+// function addEmployee() {}
+
+// Prompt for info and create a new Manager
+function addManager() {
+  console.log('Enter team manager information...');
+
+  // start screen prompts
+  inquirer
+    // get user input
+    .prompt(questionsManager)
+    // and process input
+    .then((answers) => {
+      // exctract values from answers array to create new Manager object
+      let mgr = new Manager(...Object.values(answers));
+      // push new Manager onto team array
+      team.push(mgr);
+      // console.log(team);
+      addOtherTeamMember();
     })
-  }
-
-  // function addEngineer() {}
-
-  // function addIntern() {}
-
-  // function generateHTML() {}
-
-  main();
   
+}
+
+// add more team members
+function addOtherTeamMember() {
+  console.log('Build team...');
+
+  // start screen prompts
+  inquirer
+    // get user input
+    .prompt(questionAddTeamMember)
+    // and process input
+    .then((answers) => {
+      if(answers.addChoice === "engineer"){
+        addEngineer();
+      }
+      else if(answers.addChoice === "intern") {
+        addIntern();
+      } else {
+        finishTeam();
+      };
+
+    })
+}
+
+// Call main function to start process
+main();
+
+
+//FUNCTION WISHLIST
+//------------------------------- 
+function addEngineer() {
+
+  console.log('Add an Engineer...');
+
+  // start screen prompts
+  inquirer
+    // get user input
+   .prompt(questionsEngineer)
+    // and process input
+   .then((answers) => {
+      console.log(answers);
+
+      let eng = new Engineer(...Object.values(answers));
+      // push new Engineer onto team array
+      team.push(eng);
+      console.log(team);
+      addOtherTeamMember();
+    })
+}
+
+function addIntern() {
+
+  console.log('Add an Intern...');
+
+  // start screen prompts
+  inquirer
+    // get user input
+   .prompt(questionsIntern)
+    // and process input
+   .then((answers) => {
+      console.log(answers);
+
+      let intern = new Intern(...Object.values(answers));
+      // push new Engineer onto team array
+      team.push(intern);
+      console.log(team);
+      addOtherTeamMember();
+    })
+}
+
+function finishTeam() {
+  console.log('Finished team...')
+}
+
+function generateHTML() {}
